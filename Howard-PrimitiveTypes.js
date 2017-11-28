525c65c13400319ead334d1764933047f281c7ae
Matthew Howard <mhoward14@icloud.com>
2017-11-28 16:59:00 -0700

    Adding Howard-PrimitiveTypes.js

---
+++ Howard-PrimitiveTypes.js
@@ -0,0 +1,20 @@
+// start program
+/*
+    Expected output:
+
+    Matthew
+    Howard
+    35
+    9456 Beryl Drive
+    Peyton
+    Colorado
+    80831
+*/
+
+// variable declaration and assigment goes here ...
+var firstName = "Matthew";
+
+// output
+console.log("\n")
+
+// end program
