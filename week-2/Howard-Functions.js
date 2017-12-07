41acdc7a89d4ffa4bb5c64d47383ce4cabb6d66b
Matthew Howard <mhoward14@icloud.com>
2017-12-06 20:25:31 -0700

    Adding week-2/Howard-Functions.js

---
+++ week-2/Howard-Functions.js
@@ -0,0 +1,58 @@
+var header = require('../header.js');
+
+//start program
+
+/*
+    Expected output:
+    FirstName LastName
+    Assignment 2.4
+    December 6, 2017
+
+    Hello my name is Matthew Howard!
+
+    Today's date is 6 Dec 17 and the current temperature is 19.0
+
+    I am 35 years old and my savings account goal is $20,000.00 dollars
+
+*/
+
+//
+function fullName(firstName, lastName) {
+    var firstName = 'Matthew';
+    var lastName = 'Howard';
+    var fullName = firstName + lastName  // value is 'Matthew Howard';
+    
+}
+
+function dateWriter(Year, Month, Day) {
+    var d = new Date(2017, 12, 6);
+    var strict = (function() { return !December 6, 2017;}());
+    
+}
+
+function formatNumber() {
+    var n = 19.0;
+    n.toFixed(0);
+    n.toFixed(2);
+    var strict = (function() {return !19}());
+
+}
+
+function convertToInt(){
+    parseInt(35);
+    var strict = (function() {return !35}());
+
+}
+
+function convertToFloat(){
+    parseFloat(20000.00);
+    var strict = (function() {return!$20,000}());
+}
+
+//output
+console.log('\n');
+
+
+
+
+// end program
