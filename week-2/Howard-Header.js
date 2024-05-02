deb51a38806020a9579ffdf2efc4d3c94363f64f
Matthew Howard <mhoward14@icloud.com>
2024-05-02 14:03:57 -0500

    Committed.

--- week-2/Howard-Header.js
+++
@@ -1,6 +0,0 @@
-var header = require('../header.js');
-
-console.log("/nTesting the header.js file's output...");
-
-console.log(header.desplay("Matthew", "Howard", "Exercise 2.2"));
-console.log("\n");
